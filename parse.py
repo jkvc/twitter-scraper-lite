2ff0d4e7eb9775b30018c2dfbd0ec32448e9ff3d
Junshen Kevin Chen <kevinehc@gmail.com>
2020-12-03 15:19:05 -0800

    format

--- parse.py
+++ parse.py
@@ -1,70 +1,67 @@
-import os
-import json
-import sys
-import datetime
 import argparse
-from tqdm import tqdm, trange
-from pprint import pprint
-from termcolor import colored
-from bs4 import BeautifulSoup
+import datetime
+import json
+import os
+import sys
 from concurrent.futures import ProcessPoolExecutor
+from pprint import pprint
+
+from bs4 import BeautifulSoup
+from termcolor import colored
+from tqdm import tqdm, trange
 
 from utils import *
 
-TEXT_ELEM_CLASSNAME = 'css-901oao r-hkyrab r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0'
-STATS_ELEM_CLASSNAME = 'css-1dbjc4n r-18u37iz r-1wtj0ep r-156q2ks r-1mdbhws'
-REPLY_TO_ELEM_CLASSNAME = 'css-901oao r-1re7ezh r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-qvutc0'
+TEXT_ELEM_CLASSNAME = "css-901oao r-hkyrab r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0"
+STATS_ELEM_CLASSNAME = "css-1dbjc4n r-18u37iz r-1wtj0ep r-156q2ks r-1mdbhws"
+REPLY_TO_ELEM_CLASSNAME = (
+    "css-901oao r-1re7ezh r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-qvutc0"
+)
 
 
 def parse_one_tweet(profile_name, tweet_id, raw_path):
-    raw_path = os.path.join(raw_path, profile_name, f'{tweet_id}.html')
+    raw_path = os.path.join(raw_path, profile_name, f"{tweet_id}.html")
     with open(raw_path) as f:
-        raw_html = '\n'.join(f.readlines())
-    soup = BeautifulSoup(raw_html, 'html.parser')
+        raw_html = "\n".join(f.readlines())
+    soup = BeautifulSoup(raw_html, "html.parser")
 
     # the time
-    time_elem = soup.find('time')
-    time_str = time_elem['datetime']
+    time_elem = soup.find("time")
+    time_str = time_elem["datetime"]
 
     # the text
-    text_elem = soup.find('div', {
-        'class': TEXT_ELEM_CLASSNAME
-    })
-    text_str = text_elem.text if text_elem else ''
+    text_elem = soup.find("div", {"class": TEXT_ELEM_CLASSNAME})
+    text_str = text_elem.text if text_elem else ""
 
     # the stats
     replies = retweets = likes = 0
-    stats_elem = soup.find('div', {
-        'class': STATS_ELEM_CLASSNAME
-    })
-    stats_elem_str = stats_elem['aria-label']
+    stats_elem = soup.find("div", {"class": STATS_ELEM_CLASSNAME})
+    stats_elem_str = stats_elem["aria-label"]
     if len(stats_elem_str) > 0:
-        for stat_str in stats_elem_str.split(', '):
-            stat_num, stat_name = stat_str.lower().split(' ')
+        for stat_str in stats_elem_str.split(", "):
+            stat_num, stat_name = stat_str.lower().split(" ")
             stat_num = int(stat_num)
-            if stat_name in ['replies', 'reply']:
+            if stat_name in ["replies", "reply"]:
                 replies = stat_num
-            if stat_name in ['retweets', 'retweet']:
+            if stat_name in ["retweets", "retweet"]:
                 retweets = stat_num
-            if stat_name in ['likes', 'like']:
+            if stat_name in ["likes", "like"]:
                 likes = stat_num
 
     # reply to
-    reply_to_elem = soup.find('div', {
-        'class': REPLY_TO_ELEM_CLASSNAME
-    })
+    reply_to_elem = soup.find("div", {"class": REPLY_TO_ELEM_CLASSNAME})
     if not reply_to_elem:
         reply_to = None
     else:
-        reply_to = reply_to_elem.div.a.span.text.replace('@', '')
+        reply_to = reply_to_elem.div.a.span.text.replace("@", "")
 
     parsed = {
-        'time': time_str,
-        'text': text_str,
-        'replies': replies,
-        'retweets': retweets,
-        'likes': likes,
-        'reply_to': reply_to
+        "time": time_str,
+        "text": text_str,
+        "replies": replies,
+        "retweets": retweets,
+        "likes": likes,
+        "reply_to": reply_to,
     }
     return parsed
 
@@ -72,17 +69,18 @@ def parse_one_tweet(profile_name, tweet_id, raw_path):
 def parse_one_profile(id_filepath, raw_path, dst_path, hide_progress_bar=False):
     with open(id_filepath) as f:
         id_data = json.load(f)
-    profile_name = id_data['profile_name']
-    tweet_ids = set(id_data['tweet_ids'])
+    profile_name = id_data["profile_name"]
+    tweet_ids = set(id_data["tweet_ids"])
     print(f'{colored("begin", "yellow")} [{profile_name}]')
 
     parsed_data = load_parsed_data(profile_name, dst_path)
-    parsed_tweets = parsed_data['tweets']
+    parsed_tweets = parsed_data["tweets"]
 
-    for tweet_id in tqdm(tweet_ids, desc=profile_name, disable=hide_progress_bar, leave=False):
+    for tweet_id in tqdm(
+        tweet_ids, desc=profile_name, disable=hide_progress_bar, leave=False
+    ):
         if tweet_id not in parsed_tweets:
-            parsed_tweets[tweet_id] = parse_one_tweet(
-                profile_name, tweet_id, raw_path)
+            parsed_tweets[tweet_id] = parse_one_tweet(profile_name, tweet_id, raw_path)
 
     save_parsed_data(profile_name, dst_path, parsed_data)
     print(f' {colored("done", "cyan")} [{profile_name}]')
@@ -90,38 +88,32 @@ def parse_one_profile(id_filepath, raw_path, dst_path, hide_progress_bar=False):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("meta_dir", help="dir to contain tweet ids json per profile")
+    parser.add_argument("raw_dir", help="dir to contain raw html files per tweet")
+    parser.add_argument("parsed_dir", help="dir to contain parsed json per profile")
     parser.add_argument(
-        'meta_dir', help='dir to contain tweet ids json per profile')
-    parser.add_argument(
-        'raw_dir', help='dir to contain raw html files per tweet')
-    parser.add_argument(
-        'parsed_dir', help='dir to contain parsed json per profile')
-    parser.add_argument(
-        '-w', '--workers',
-        help='number of process, default 1',
-        type=int,
-        default=1
+        "-w", "--workers", help="number of process, default 1", type=int, default=1
     )
     args = parser.parse_args()
 
-    meta_filepaths = sorted(list(
-        os.path.join(args.meta_dir, file)
-        for file in os.listdir(args.meta_dir)
-        if os.path.isfile(os.path.join(args.meta_dir, file))
-    ))
+    meta_filepaths = sorted(
+        list(
+            os.path.join(args.meta_dir, file)
+            for file in os.listdir(args.meta_dir)
+            if os.path.isfile(os.path.join(args.meta_dir, file))
+        )
+    )
 
     if args.workers == 1:
         for i, meta_filepath in enumerate(meta_filepaths):
-            parse_one_profile(
-                meta_filepath,
-                args.raw_dir,
-                args.parsed_dir,
-                False
-            )
+            parse_one_profile(meta_filepath, args.raw_dir, args.parsed_dir, False)
     else:
         with ProcessPoolExecutor(max_workers=args.workers) as exe:
             for i, meta_filepath in enumerate(meta_filepaths):
                 exe.submit(
                     parse_one_profile,
-                    meta_filepath, args.raw_dir, args.parsed_dir, True
+                    meta_filepath,
+                    args.raw_dir,
+                    args.parsed_dir,
+                    True,
                 )
--- scrape.py
+++ scrape.py
@@ -1,36 +1,39 @@
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, WebDriverException
-from selenium.webdriver.chrome.options import Options
-from time import sleep
-from tqdm import tqdm
+import argparse
+import datetime
+import json
+import os
+import sys
 from concurrent.futures import ProcessPoolExecutor
 from pprint import pprint
+from time import sleep
+
+from selenium import webdriver
+from selenium.common.exceptions import (
+    NoSuchElementException,
+    StaleElementReferenceException,
+    WebDriverException,
+)
+from selenium.webdriver.chrome.options import Options
 from termcolor import cprint
-import json
-import datetime
-import os
-import argparse
-import sys
+from tqdm import tqdm
 
 from utils import *
 
-
-DEFAULT_BEGIN_DATE = '2010-01-01'
+DEFAULT_BEGIN_DATE = "2010-01-01"
 DEFAULT_DAYS_PER_SEARCH = 10
 
 PAGE_DELAY = 1  # seconds
 RATE_LIMITED_DELAY = 60  # second
 BASEURL = (
-    'https://twitter.com/search?q=' +
-    'from%3A{}%20' +
-    'since%3A{}%20' +
-    'until%3A{}%20' +
-    'include%3Aretweets&src=typed_query&f=live'
+    "https://twitter.com/search?q="
+    + "from%3A{}%20"
+    + "since%3A{}%20"
+    + "until%3A{}%20"
+    + "include%3Aretweets&src=typed_query&f=live"
 )
 
 CHROMEDRIVER_PATH = os.path.join(
-    os.path.dirname(os.path.realpath(__file__)),
-    'chromedriver'
+    os.path.dirname(os.path.realpath(__file__)), "chromedriver"
 )
 CHROME_OPTIONS = Options()
 # TWEET_SELECTOR css depends on window width
@@ -39,23 +42,25 @@ CHROME_OPTIONS.add_argument("--window-size=800,2000")
 prefs = {"profile.managed_default_content_settings.images": 2}
 CHROME_OPTIONS.add_experimental_option("prefs", prefs)
 
-TWEET_SELECTOR = 'div.css-1dbjc4n.r-eqz5dr.r-16y2uox.r-1wbh5a2'
-ID_SELECTOR ='a.css-4rbku5.css-18t94o4.css-901oao.r-m0bqgq.r-1loqt21.r-1q142lx.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-3s2u2q.r-qvutc0' 
-NO_RESULT_SELECTOR = 'div.css-901oao.r-18jsvk2.r-1qd0xha.r-1b6yd1w.r-b88u0q.r-ad9z0x.r-15d164r.r-bcqeeo.r-q4m81j.r-qvutc0' 
+TWEET_SELECTOR = "div.css-1dbjc4n.r-eqz5dr.r-16y2uox.r-1wbh5a2"
+ID_SELECTOR = "a.css-4rbku5.css-18t94o4.css-901oao.r-m0bqgq.r-1loqt21.r-1q142lx.r-1qd0xha.r-a023e6.r-16dba41.r-ad9z0x.r-bcqeeo.r-3s2u2q.r-qvutc0"
+NO_RESULT_SELECTOR = "div.css-901oao.r-18jsvk2.r-1qd0xha.r-1b6yd1w.r-b88u0q.r-ad9z0x.r-15d164r.r-bcqeeo.r-q4m81j.r-qvutc0"
 
 ########################################################################
 
 
 def build_url(profile_name, begin_date, end_date):
-    begin_str = begin_date.strftime('%Y-%m-%d')
-    end_str = end_date.strftime('%Y-%m-%d')
+    begin_str = begin_date.strftime("%Y-%m-%d")
+    end_str = end_date.strftime("%Y-%m-%d")
     return BASEURL.format(profile_name, begin_str, end_str)
 
 
 def get_tweet_id(tweet_elem):
-    tweet_id = tweet_elem.find_element_by_css_selector(
-        ID_SELECTOR
-    ).get_attribute('href').split('/')[-1]
+    tweet_id = (
+        tweet_elem.find_element_by_css_selector(ID_SELECTOR)
+        .get_attribute("href")
+        .split("/")[-1]
+    )
     return tweet_id
 
 
@@ -85,7 +90,7 @@ def scrape_one_page(driver, url, profile_name, raw_dir, max_scroll_iter=3000):
     try:
         scroll_to_top(driver)
         prev_scroll_height = 0
-        sleep(.1)
+        sleep(0.1)
 
         for _ in range(max_scroll_iter):
             tweet_elems = driver.find_elements_by_css_selector(TWEET_SELECTOR)
@@ -93,14 +98,14 @@ def scrape_one_page(driver, url, profile_name, raw_dir, max_scroll_iter=3000):
                 try:
                     tweet_id = get_tweet_id(elem)
                     if tweet_id not in tweet_ids:
-                        raw_html_str = elem.get_attribute('outerHTML')
+                        raw_html_str = elem.get_attribute("outerHTML")
                         save_raw(tweet_id, raw_html_str, raw_dir, profile_name)
                         tweet_ids.add(tweet_id)
                 except (StaleElementReferenceException, WebDriverException) as e:
                     continue
 
             scroll_down_viewheight(driver)
-            sleep(.1)
+            sleep(0.1)
             curr_scroll_height = get_curr_scroll_height(driver)
 
             if curr_scroll_height <= prev_scroll_height:
@@ -111,7 +116,7 @@ def scrape_one_page(driver, url, profile_name, raw_dir, max_scroll_iter=3000):
         pass
 
     if is_rate_limited(driver, tweet_ids):
-        cprint(f'rate limited, {profile_name}', 'red')
+        cprint(f"rate limited, {profile_name}", "red")
         driver.delete_all_cookies()
         sleep(RATE_LIMITED_DELAY)
         return None
@@ -120,25 +125,27 @@ def scrape_one_page(driver, url, profile_name, raw_dir, max_scroll_iter=3000):
     return tweet_ids
 
 
-def scrape_one_profile(profile_name, begin_date_str, days_per_search, meta_dir, raw_dir):
+def scrape_one_profile(
+    profile_name, begin_date_str, days_per_search, meta_dir, raw_dir
+):
     driver = get_driver(CHROME_OPTIONS)
 
     data = load_metadata(profile_name, begin_date_str, meta_dir)
     cprint(
         f'start scraping [{profile_name}], already has [{len(data["tweet_ids"])}] tweets, latest at [{data["latest_date"]}]',
-        'yellow')
-    tweet_ids = set(data['tweet_ids'])
+        "yellow",
+    )
+    tweet_ids = set(data["tweet_ids"])
 
-    begin_date = datetime.datetime.strptime(begin_date_str, '%Y-%m-%d')
-    if 'latest_date' in data:
+    begin_date = datetime.datetime.strptime(begin_date_str, "%Y-%m-%d")
+    if "latest_date" in data:
         begin_date = max(
-            datetime.datetime.strptime(data['latest_date'], '%Y-%m-%d'),
-            begin_date
+            datetime.datetime.strptime(data["latest_date"], "%Y-%m-%d"), begin_date
         )
     date_ranges = build_date_ranges(
         begin_date,
         datetime.datetime.now() + datetime.timedelta(days=2),
-        days_per_search
+        days_per_search,
     )
 
     for begin_date, end_date in tqdm(date_ranges, desc=profile_name):
@@ -154,19 +161,19 @@ def scrape_one_profile(profile_name, begin_date_str, days_per_search, meta_dir, 
                 break
 
         tweet_ids = tweet_ids.union(new_tweet_ids)
-        data['tweet_ids'] = list(tweet_ids)
+        data["tweet_ids"] = list(tweet_ids)
 
         # checkpoint the latest date
         latest_date = min(
-            end_date,
-            datetime.datetime.now() - datetime.timedelta(days=1)  # yesterday
-        ).strftime('%Y-%m-%d')
-        data['latest_date'] = latest_date
+            end_date, datetime.datetime.now() - datetime.timedelta(days=1)  # yesterday
+        ).strftime("%Y-%m-%d")
+        data["latest_date"] = latest_date
 
         save_metadata(profile_name, data, meta_dir)
 
     cprint(
-        f'done scraping [{profile_name}] with [{len(data["tweet_ids"])}] tweets', 'cyan')
+        f'done scraping [{profile_name}] with [{len(data["tweet_ids"])}] tweets', "cyan"
+    )
 
     driver.close()
 
@@ -174,38 +181,28 @@ def scrape_one_profile(profile_name, begin_date_str, days_per_search, meta_dir, 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        'profiles', help='file containing profile strings, one per line')
-    parser.add_argument(
-        'meta_dir', help='dir to contain tweet ids json per profile')
-    parser.add_argument(
-        'raw_dir', help='dir to contain raw html files per tweet')
-    parser.add_argument(
-        '-w', '--workers',
-        type=int,
-        default=1
+        "profiles", help="file containing profile strings, one per line"
     )
+    parser.add_argument("meta_dir", help="dir to contain tweet ids json per profile")
+    parser.add_argument("raw_dir", help="dir to contain raw html files per tweet")
+    parser.add_argument("-w", "--workers", type=int, default=1)
     parser.add_argument(
-        '-q', '--quiet',
+        "-q",
+        "--quiet",
         help="don't show browser window",
-        action='store_true',
+        action="store_true",
     )
-    parser.add_argument(
-        '-p', '--proxy',
-        help='proxy address and port'
-    )
+    parser.add_argument("-p", "--proxy", help="proxy address and port")
     args = parser.parse_args()
 
     with open(args.profiles) as f:
-        profile_names = [
-            s.rstrip()
-            for s in f.readlines()
-        ]
+        profile_names = [s.rstrip() for s in f.readlines()]
 
     if args.quiet:
         CHROME_OPTIONS.add_argument("--headless")
 
     if args.proxy:
-        CHROME_OPTIONS.add_argument(f'--proxy-server={args.proxy}')
+        CHROME_OPTIONS.add_argument(f"--proxy-server={args.proxy}")
 
     if args.workers > 1:
         with ProcessPoolExecutor(max_workers=args.workers) as exe:
@@ -217,7 +214,7 @@ if __name__ == "__main__":
                     DEFAULT_BEGIN_DATE,
                     DEFAULT_DAYS_PER_SEARCH,
                     args.meta_dir,
-                    args.raw_dir
+                    args.raw_dir,
                 )
     else:
         for profile_name in profile_names:
